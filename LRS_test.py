7f135c6ce9ad579894c6dd41fea58c7670a61720
Eric Conklin <eric.s.conklin@gmail.com>
2016-04-21 20:24:45 -0400

    moved connection data to git-ignored json file

--- LRS_test.py
+++ LRS_test.py
@@ -1,7 +1,18 @@
-import psycopg2
+import psycopg2, json
 from LrsThread import LrsThread, initLrsThread
 
 def main():
+    connectionData = None
+    try:
+        connectionData = getConnectionData()
+    except:
+        pass
+    
+    if not connectionData:
+        print "Could not read file: 'connectionData.json'"
+        print "Exiting..."
+        return
+    
     connection = psycopg2.connect(**connectionData)
     cursor = connection.cursor()
     
@@ -18,9 +29,13 @@ def main():
         
     for thread in threads:        
         thread.join()
-    
+        
     connection.commit()
     connection.close()
+    
+def getConnectionData():
+    with open("./connectionData.json") as jsonFile:
+        return json.load(jsonFile, encoding="utf-8")
     
 def getLinkIds(cursor):
     sql = '''
@@ -29,7 +44,7 @@ def getLinkIds(cursor):
         WHERE st_name LIKE 'WOLF RD'
         '''
     cursor.execute(sql)
-    return [ result[0] for result in cursor ]
+    return [ int(result[0]) for result in cursor ]
 
 if __name__ == "__main__":
     main()
